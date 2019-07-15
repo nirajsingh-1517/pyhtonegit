0ac8c2a92ea6ecb06847ddc1d79b7874f40256da
nirajsingh-1517 <47865691+nirajsingh-1517@users.noreply.github.com>
2019-07-15 14:06:28 +0530

    Create democlass.py

---
+++ democlass.py
@@ -0,0 +1,11 @@
+Class Student:
+  def __init__(self, first, last, marks)
+   self.first = first
+   self.last = last
+   self.marks = marks
+
+Std_1 = Student('Niraj', 'Singh', 70)
+Std_2 = Student('Subham', 'Tiwari', 75)
+
+print(Std_1.first)
+print(Std_2.first)
